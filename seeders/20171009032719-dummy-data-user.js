ea8917771e52b152d01409be97f9b1efb5276474
Nandira Paturohman <nandirasp@gmail.com>
2017-10-10 01:25:18 +0700

    final

--- seeders/20171009032719-dummy-data-user.js
+++ seeders/20171009032719-dummy-data-user.js
@@ -12,7 +12,7 @@ module.exports = {
         password: '52746179fc62cc114c0d9c59186907913669ac38030e3b3a3572470b23a30e53',
         role: 'academic',
         secret: 'ctQ2tQHk'
-      },{No limit
+      },{
         username: 'charlesxavier',
         password: '15908e11ed1d5e1b6d608b2afd6c78e7cfc04e59f554a38f61ef7c15229b89b1',
         role: 'headmaster',
